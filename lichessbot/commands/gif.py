346e34dc42b6154832ad74aa1ed92310e45df9be
adenzu <erencakar54@gmail.com>
2021-05-10 15:16:35 +0300

    added user id parameter to gif command

--- lichessbot/commands/gif.py
+++ lichessbot/commands/gif.py
@@ -1,7 +1,7 @@
 from lichessbot.command import Command
-from lichessbot.client import client
 from lichessbot.exceptions import *
 from lichessbot.parameter import *
+from lichessbot.client import *
 from lichessbot.util import *
 
 import berserk
@@ -11,15 +11,29 @@ class CommandGif(Command):
 	name = "gif"
 	help_string = "View gif of a chess game."
 	aliases = []
-	parameters = [ParamGameID(), ParamColor(required=False)]
+	parameters = [ParamUnion(ParamGameID(), ParamUserID()), ParamColor(required=False)]
 
 	@classmethod
 	async def run(cls, command_call):
 
-		try:
-			game = client.games.export(command_call.args[0])
-			await command_call.channel.send(f"https://lichess1.org/game/export/gif{command_call.args[1]}/{command_call.args[0]}.gif")
-		except berserk.exceptions.ResponseError:
-			await command_call.channel.send(f"No such game is played on lichess with id `{command_call.args[0]}`")
+		parsed_class = cls.parameters[0].get_parsed_class()
+		game_id = command_call.args[0]
+		
+		if parsed_class == ParamUserID:
+			
+			user_id = command_call.args[0]
+			user_info = client.users.get_public_data(user_id)
 
+			if "playing" in user_info:
+				await command_call.channel.send(f"User `{user_id}` is currently playing a game. Use `!lc watch {user_id}` instead.")
+				return
+
+			player_game_gen = get_user_games(user_id)
+			game_id = next(player_game_gen)["id"]
+			
+		try:
+			game = client.games.export(game_id)
+			await command_call.channel.send(f"https://lichess1.org/game/export/gif{command_call.args[1]}/{game_id}.gif")
+		except berserk.exceptions.ResponseError:
+			await command_call.channel.send(f"No such game is played on lichess with id `{game_id}`")
 
